0b9cf82a4de844d70a11e23416d739a24cd3df1c
brxwnsugxr <jsdev.mas@gmail.com>
2023-12-10 00:12:53 +1100

    condition if tie

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,24 @@
 // creating multipl smaller functions for easier readability
 
-//function to get user choice 
+//function to get user choice
 const getUserChoice = function () {
   const userChoice = window.prompt('enter R,P ior S:');
-//   ternany operator check if userchoice is truthy. converts the input to uppercase, if false exit
+  //   ternany operator check if userchoice is truthy. converts the input to uppercase, if false exit
   return userChoice ? userChoice.toUpperCase() : null;
 };
 
+// function to compare choices
+const compareChoice = function (userChoice, aiChoice) {
+  // win condition
+  const winConditions = {
+    RS: true,
+    SR: true,
+    PR: true,
+    RP: true,
+    SP: true,
+    PS: true,
+  };
+
+  if (userChoice === aiChoice) {
+    window.alert('its a tie');
+    return 'tie';
