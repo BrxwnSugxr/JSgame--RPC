7d4538caaff1f0cc3c7b4b9ad47894f20404f90c
brxwnsugxr <jsdev.mas@gmail.com>
2023-12-09 12:32:12 +1100

    converting uppercase so easy comparison

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,18 @@
-// declaring varaibles 
+// declaring varaibles
 let win = 0;
 let lose = 0;
 let tie = 0;
 
 let options = ['R', 'P', 'S'];
-//creating a function to allow user to input "options" 
+//creating a function to allow user to input "options"
 var playGame = function () {
   var userChoice = window.prompt('Enter R, P, or S:');
-// if user doesnt input anything function exits 
+  // if user doesnt input anything function exits
   if (!userChoice) {
     return;
   }
-// adding a contion of user input 
+  // adding a constraint of user input to allow comparison easier
+  userChoice = userChoice.toUpperCase;
 
-
-
-
+  
 };
