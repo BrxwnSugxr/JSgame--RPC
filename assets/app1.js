c840925e44580ffef9fc06b393ccde8aeb17d716
brxwnsugxr <jsdev.mas@gmail.com>
2023-12-10 13:13:11 +1100

    connecting the html css and jquery all together

--- assets/app1.js
+++ assets/app1.js
@@ -1,2 +1,13 @@
-// this is now the UI interaction 
-
+// this is now the UI interaction
+// get all the selection button, based on the data attributes
+const selectionButtons = document.querySelectorAll('[data-selection');
+selectionButtons.forEach((selectionButtons) => {
+  selectionButtons.addEventListener('click', (e) => {
+    const selectionName = selectionButtons.dataset.selection;
+    makeSelection(selectionName);
+  });
+});
+// displaying
+function makeSelection(selection) {
+  console.log(selection);
+}
