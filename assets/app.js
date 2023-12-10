7727516b87cf42e5a318d54da9d20ad44a851323
brxwnsugxr <jsdev.mas@gmail.com>
2023-12-10 12:40:17 +1100

    creating new app1.js as first js file is for prompt second file is for UI interaction

--- assets/app.js
+++ assets/app.js
@@ -1,74 +1,76 @@
-// creating multipl smaller functions for easier readability
+//  this is rock paper scisors based on user prompt
 
-//function to get user choice
-const getUserChoice = function () {
-  const userChoice = window.prompt('enter R,P ior S:');
-  //   ternany operator check if userchoice is truthy. converts the input to uppercase, if false exit
-  return userChoice ? userChoice.toUpperCase() : null;
-};
+// // creating multipl smaller functions for easier readability
 
-const getComputerChoice = function () {
-  const options = ['R', 'P', 'S'];
-  const index = Math.floor(Math.random() * options.length);
-  return options[index];
-};
+// //function to get user choice
+// const getUserChoice = function () {
+//   const userChoice = window.prompt('enter R,P ior S:');
+//   //   ternany operator check if userchoice is truthy. converts the input to uppercase, if false exit
+//   return userChoice ? userChoice.toUpperCase() : null;
+// };
 
-// function to compare choices
-const compareChoice = function (userChoice, aiChoice) {
-  // win condition
-  const winConditions = {
-    RS: true,
-    SR: true,
-    PR: true,
-    RP: true,
-    SP: true,
-    PS: true,
-  };
+// const getComputerChoice = function () {
+//   const options = ['R', 'P', 'S'];
+//   const index = Math.floor(Math.random() * options.length);
+//   return options[index];
+// };
 
-  if (userChoice === aiChoice) {
-    window.alert('its a tie');
-    return 'tie';
-  } else if (winConditions[userChoice + aiChoice]) {
-    window.alert('you win');
-    return 'win';
-  } else {
-    window.alert('you lose');
-    return 'lose';
-  }
-};
+// // function to compare choices
+// const compareChoice = function (userChoice, aiChoice) {
+//   // win condition
+//   const winConditions = {
+//     RS: true,
+//     SR: true,
+//     PR: true,
+//     RP: true,
+//     SP: true,
+//     PS: true,
+//   };
 
-const displayResults = function (win, lose, tie) {
-  window.alert(`Status: \nWins: ${win} \nlose: ${lose} \ntie: ${tie} `);
-};
+//   if (userChoice === aiChoice) {
+//     window.alert('its a tie');
+//     return 'tie';
+//   } else if (winConditions[userChoice + aiChoice]) {
+//     window.alert('you win');
+//     return 'win';
+//   } else {
+//     window.alert('you lose');
+//     return 'lose';
+//   }
+// };
 
-const askToPlayAgain = function () {
-  return window.confirm('Play again?');
-};
+// const displayResults = function (win, lose, tie) {
+//   window.alert(`Status: \nWins: ${win} \nlose: ${lose} \ntie: ${tie} `);
+// };
 
-const playGame = function () {
-  let win = 0;
-  let lose = 0;
-  let tie = 0;
+// const askToPlayAgain = function () {
+//   return window.confirm('Play again?');
+// };
 
-  do {
-    const userChoice = getUserChoice();
-    if (userChoice === null) {
-      return;
-    }
-    const aiChoice = getComputerChoice();
-    window.alert(`AI chose: ${aiChoice}`);
+// const playGame = function () {
+//   let win = 0;
+//   let lose = 0;
+//   let tie = 0;
 
-    const result = compareChoice(userChoice, aiChoice);
-    if (result === 'win') {
-      win++;
-    } else if (result === 'lose') {
-      lose++;
-    } else {
-      tie++;
-    }
-    displayResults(win, lose, tie);
-  } while (askToPlayAgain());
-  window.alert('thanks for playing');
-};
+//   do {
+//     const userChoice = getUserChoice();
+//     if (userChoice === null) {
+//       return;
+//     }
+//     const aiChoice = getComputerChoice();
+//     window.alert(`AI chose: ${aiChoice}`);
 
-playGame();
+//     const result = compareChoice(userChoice, aiChoice);
+//     if (result === 'win') {
+//       win++;
+//     } else if (result === 'lose') {
+//       lose++;
+//     } else {
+//       tie++;
+//     }
+//     displayResults(win, lose, tie);
+//   } while (askToPlayAgain());
+//   window.alert('thanks for playing');
+// };
+
+// playGame();
---
+++ assets/app1.js
@@ -0,0 +1,2 @@
+// this is now the UI interaction 
+
