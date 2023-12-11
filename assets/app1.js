77ffa834c6bd263dbcaeaaccf28d46aed76ae399
brxwnsugxr <jsdev.mas@gmail.com>
2023-12-11 14:19:55 +1100

    creating the object

--- assets/app1.js
+++ assets/app1.js
@@ -1,6 +1,29 @@
 // this is now the UI interaction
 // get all the selection button, based on the data attributes
 const selectionButtons = document.querySelectorAll('[data-selection');
+const selections = [
+  {
+    name: 'rock',
+    emoji: '✊🏾',
+    beats: 'scissor',
+  },
+  {
+    name: 'rock',
+    emoji: '✊🏾',
+    beats: 'scissor',
+  },
+  {
+    name: 'paper',
+    emoji: '🖐🏾',
+    beats: 'rock',
+  },
+  {
+    name: 'scissor',
+    emoji: '✌🏾',
+    beats: 'paper',
+  },
+];
+
 selectionButtons.forEach((selectionButtons) => {
   selectionButtons.addEventListener('click', (e) => {
     const selectionName = selectionButtons.dataset.selection;
